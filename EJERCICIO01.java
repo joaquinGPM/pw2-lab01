49f1babcbfcabcf8f2113c46b7ee8c70ca425a0d
joaquinGPM <91225726+joaquinGPM@users.noreply.github.com>
2022-04-29 20:53:41 -0500

    Create EJERCICIO01.java

---
+++ EJERCICIO01.java
@@ -0,0 +1,5 @@
+public static void main(String[]args){
+  System.out.println("¡HOLA MUNDO!");
+  System.out.println("Bienvenidos al curso de Programacion WEB2");
+  System.out.println("Echo por: Joaquín Gonzalo Paredes Mescco");
+}
